97a193a7de3f829ca76eb5adef3f27d63bf48f12
Elias0099 <103077355+Elias0099@users.noreply.github.com>
2023-06-27 20:00:18 -0500

    gatewawy

---
+++ ms-admin-api-gateway/src/main/java/pe/edu/galaxy/training/java/ms/sc/ventas/apigateway/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package pe.edu.galaxy.training.java.ms.sc.ventas.apigateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TokenDto {
+
+    private String token;
+
+}
